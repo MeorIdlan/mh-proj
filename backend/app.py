850f2e6a5d7a9ef65370bd694a47b18652eeeefe
MeorIdlan <75961990+MeorIdlan@users.noreply.github.com>
2024-06-25 11:15:26 +0800

    Create new backend route for complex query
    - added new backend route
    - refactor code in backend to use Location class
    - use flask_cors for CORS instead of adding to response header
    - reworked search filter on frontend to accommodate for new backend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from scraper import scrape
+from location.locationObj import Location
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/subway-locations', methods=['GET'])
 def get_subway_locations():
@@ -13,9 +16,18 @@ def get_subway_locations():
 def dev_get_subway_locations():
     query = request.args.get('query', default=None, type=str)
     scraper = scrape.Scraper('dev')
-    response = jsonify({'locations': scraper.getLocations(query=query)})
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return jsonify({'locations': scraper.getLocations(query=query)})
+
+@app.route('/api/complex-query', methods=['POST'])
+def complex_query():
+    data = request.json
+    query = data['query'].lower()
+    location = Location.fromDict(data['location'])
+    
+    # here, call function to determine if location matches query
+    
+    # for now return false
+    return jsonify({'include': False, 'request': data})
 
 if __name__ == '__main__':
     app.run(debug=True)
